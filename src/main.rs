845b9c4950eb15954e569dfef0de531e52066977
Otavio Jacobi <otaviojacobi@gmail.com>
2026-06-28 20:40:04 -0300

    feat: park other cores whie booting

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 #![no_std]
 #![no_main]
+use core::arch::global_asm;
 use core::panic::PanicInfo;
 use core::ptr::write_volatile;
 
 const UART_DR: *mut u32 = 0xFE20_1000 as *mut u32;
 
+global_asm! {
+    r#"
+    .section ".text._start"
+    .global _start
+    _start:
+        MRS x1, MPIDR_EL1
+        AND x1, x1, #0xFF
+        CBNZ x1, _loop_other_core
+        B start_kernel
+    _loop_other_core:
+        WFE
+        B _loop_other_core
+    "#
+}
+
 #[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn start_kernel() -> ! {
     unsafe {
         write_volatile(UART_DR, b'o' as u32);
     }
