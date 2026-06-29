94c6b07b146ce7928b30f6dfb126cc3433b873ef
Otavio Jacobi <otaviojacobi@gmail.com>
2026-06-29 13:31:30 -0300

    feat: add 8 aligned .__bss_start and .__bss_end

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use core::panic::PanicInfo;
 use core::ptr::write_volatile;
 
 const UART_DR: *mut u32 = 0xFE20_1000 as *mut u32;
+static mut VAR: u32 = 0;
+static mut VAR1: u32 = 2;
+
 
 global_asm!(include_str!(env!("PAMPA_BOOT_ASM")));
 
@@ -12,6 +15,9 @@ global_asm!(include_str!(env!("PAMPA_BOOT_ASM")));
 pub extern "C" fn start_kernel() -> ! {
     unsafe {
         write_volatile(UART_DR, b'o' as u32);
+        core::ptr::write_volatile(&raw mut VAR, 1);
+        core::ptr::write_volatile(&raw mut VAR1, 1);
+
     }
     loop {}
 }
