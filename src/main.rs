cb63566c4080115b3f506b6c71c1a42f4736d338
Otavio Jacobi <otaviojacobi@gmail.com>
2026-06-28 21:00:21 -0300

    feat: add stack

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ global_asm! {
         MRS x1, MPIDR_EL1
         AND x1, x1, #0xFF
         CBNZ x1, _loop_other_core
+        MOV x1, #0x80000
+        MOV sp, x1
         B start_kernel
     _loop_other_core:
         WFE
