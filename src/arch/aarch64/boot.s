3403dc83c919c74bf0507bbc3e46f6faa5b3913a
Otavio Jacobi <otaviojacobi@gmail.com>
2026-06-29 11:55:12 -0300

    refactor: use boot.s __stack_top
    Increases a bit further future possible device types.
    Note that the Aff0 mask #0xFF stays the same - for more dts (other than the raspberrypi family)
    we would probably need more sophisticated code to get the correct cores to park/start

--- src/arch/aarch64/boot.s
+++ src/arch/aarch64/boot.s
@@ -4,7 +4,7 @@ _start:
     MRS x1, MPIDR_EL1
     AND x1, x1, #0xFF
     CBNZ x1, _loop_other_core
-    MOV x1, #0x80000
+    LDR x1, =__stack_top
     MOV sp, x1
     B start_kernel
 _loop_other_core:
