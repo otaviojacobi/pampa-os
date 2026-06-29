4cc81488c1b22ecf34c38721250226b631f7d019
Otavio Jacobi <otaviojacobi@gmail.com>
2026-06-29 16:55:34 -0300

    feat: redirect interrupts to handle_cpu_exception, add uart drive

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,43 @@
 #![no_std]
 #![no_main]
 use core::arch::global_asm;
+use core::fmt::Write;
 use core::panic::PanicInfo;
-use core::ptr::write_volatile;
 
-const UART_DR: *mut u32 = 0xFE20_1000 as *mut u32;
-static mut VAR: u32 = 0;
-static mut VAR1: u32 = 2;
-
+use crate::drivers::uart::Uart;
 
 global_asm!(include_str!(env!("PAMPA_BOOT_ASM")));
 
+mod drivers;
+
 #[unsafe(no_mangle)]
 pub extern "C" fn start_kernel() -> ! {
+    let mut uart = Uart;
+    let _ = write!(uart, "hello, otavio\n   ");
     unsafe {
-        write_volatile(UART_DR, b'o' as u32);
-        core::ptr::write_volatile(&raw mut VAR, 1);
-        core::ptr::write_volatile(&raw mut VAR1, 1);
-
+        core::arch::asm!("svc #0");
     }
     loop {}
 }
 
+#[unsafe(no_mangle)]
+pub extern "C" fn handle_cpu_exception() -> ! {
+    let esr: u64;
+    let far: u64;
+    let elr: u64;
+
+    unsafe {
+        core::arch::asm!("MRS {}, ESR_EL1", out(reg) esr);
+        core::arch::asm!("MRS {}, FAR_EL1", out(reg) far);
+        core::arch::asm!("MRS {}, ELR_EL1", out(reg) elr);
+    }
+
+    panic!("out esr={esr:#x}, far={far:#x}, elr={elr:#x}");
+}
+
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    let mut uart = Uart;
+    let _ = write!(uart, "{info}");
     loop {}
 }
